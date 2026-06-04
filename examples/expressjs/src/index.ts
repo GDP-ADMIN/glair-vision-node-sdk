df46b811b135e251dc352f07b28002589ab979ce
CleveeJ <willsoncj285@gmail.com>
2026-06-04 16:43:21 +0700

    feat(general-document): add general document OCR endpoint and integrate with frontend

--- examples/expressjs/src/index.ts
+++ examples/expressjs/src/index.ts
@@ -51,6 +51,35 @@ app.post("/pl", async (req: Request, res: Response) => {
   res.status(200).json(response);
 });
 
+app.post("/general-document", async (req: Request, res: Response) => {
+  const form = formidable({ multiples: true });
+  const parse = (): Promise<any> => {
+    return new Promise((resolve, reject) => {
+      form.parse(req, async (err: any, fields: any, files: any) => {
+        if (err) {
+          reject("request body parse error");
+        }
+        resolve({ fields, files });
+      });
+    });
+  };
+
+  const { files } = await parse();
+
+  const filePath = (files.image as any)[0].filepath;
+
+  let error = null;
+  const response = await vision()
+    .ocr.generalDocument({ image: filePath })
+    .catch((err) => (error = err));
+
+  if (error) {
+    res.status(400).json(error);
+    return;
+  }
+  res.status(200).json(response);
+});
+
 app.post("/pl-sessions", async (req: Request, res: Response) => {
   let error = null;
 
